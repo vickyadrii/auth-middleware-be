1f244b36df942d0e15e2e20314ffb8c00a6df61b
vickyadri29 <vickyadri290501@gmail.com>
2024-05-22 14:30:20 +0800

    refactor: adjust codes

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -2,6 +2,6 @@ import { Request, Response } from "express";
 
 export const getDefaultRoute = (req: Request, res: Response) => {
   res.status(200).json({
-    message: "Hello World!!!",
+    message: "Hello World!",
   });
 };
