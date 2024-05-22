57240a31bba1556ee0669c5757ee852496140594
vickyadri29 <vickyadri290501@gmail.com>
2024-05-22 14:08:27 +0800

    feat(init): init all files

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Hello World",
+    data: [],
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
