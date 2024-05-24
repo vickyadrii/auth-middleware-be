6b8389679e8f425b230bfba58537e22ded3e2efb
vickyadri29 <vickyadri290501@gmail.com>
2024-05-24 13:19:51 +0800

    fix(cors): handle cors

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,25 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import router from "./routes";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const options: cors.CorsOptions = {
+  origin: "*",
+  methods: [],
+  allowedHeaders: [],
+  exposedHeaders: [],
+  credentials: true,
+};
 
+app.options("*", cors());
+app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(router)
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
