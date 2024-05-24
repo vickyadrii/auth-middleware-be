7bb6ac7cde440142f575bb6df33ea946debf8876
vickyadri29 <vickyadri290501@gmail.com>
2024-05-24 11:18:07 +0800

    feat(user): add middleware on endpoint update user

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,8 +1,9 @@
+import { authMiddleware } from "../middleware/authMiddleware";
 import { updateUser } from "../controllers/userController";
 import { Router } from "express";
 
 const router = Router();
 
-router.put("/update", updateUser);
+router.put("/update", authMiddleware, updateUser);
 
 export default router;
